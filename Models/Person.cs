3f465c6bef452ba3dd1e50991530991cbc634ef2
Stefan Balaban <stefanbalaban97@gmail.com>
2019-09-13 23:18:19 +0200

    Added test project

---
+++ Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pipelines_dotnet_core.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string IntroduceTo(string name)
+        {
+            return $"Hi {name}, I'm {Name}";
+        }
+    }
+}
