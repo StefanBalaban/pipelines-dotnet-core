00b15d16cc1b8a644aea1ecb3555f37c72950865
Stefan Balaban <stefanbalaban97@gmail.com>
2019-09-13 23:33:46 +0200

    test

---
+++ PipelinesDotNetCoreTests/PersonUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using pipelines_dotnet_core.Models;
+
+namespace PipelinesTests
+{
+    [TestFixture]
+    class PersonUnitTests
+    {
+        [Test]
+        public void IntroduceTo_CalledWithJohnAndSmith_ContainsJohnAndSmith()
+        {
+            var person = new Person();
+            person.Name = "John";
+
+            var result = person.IntroduceTo("Smith");
+
+
+            Assert.That(result, Does.Contain("John").IgnoreCase);
+            Assert.That(result, Does.Contain("Smith").IgnoreCase);
+        }
+    }
+}
